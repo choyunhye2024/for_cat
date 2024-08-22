420806a7baf1eeb2faf117a35ef5cc9df1ed7496
choyunhye2024 <jdbsgp1221@gmail.com>
2024-08-22 09:34:10 +0900

    Add files via upload

---
+++ For/src/com/for_/Cat.java
@@ -0,0 +1,16 @@
+package com.for_;
+
+public class Cat {
+
+	String name;
+	int age;
+	
+	//생성자 함수를 자동으로 불러오는 법: 오른쪽마우스 - source - Generate Constructor using Fields - Generate
+
+	public Cat(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+}
